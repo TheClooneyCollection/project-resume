3b44f2a11aac3322ee0e173107f3e4d824ead8fe
NicholasClooney <137233165+NicholasClooney@users.noreply.github.com>
2025-11-05 10:03:01 +0000

    Use set

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,21 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addDataExtension("yaml", (contents) => yaml.load(contents));
   eleventyConfig.addDataExtension("yml", (contents) => yaml.load(contents));
   eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addFilter("hasIntersection", (first = [], second = []) => {
+    if (!Array.isArray(first) || !Array.isArray(second)) {
+      return false;
+    }
+    if (!first.length || !second.length) {
+      return false;
+    }
+    const firstSet = new Set(first);
+    for (const value of second) {
+      if (firstSet.has(value)) {
+        return true;
+      }
+    }
+    return false;
+  });
 
   eleventyConfig.on("eleventy.after", () => {
     return new Promise((resolve, reject) => {
